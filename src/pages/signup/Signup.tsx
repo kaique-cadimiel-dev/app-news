de9d341dc2a324a02fecba8e862161116abe9e9a
kaique-cadimiel-dev <kaiqueamasio@gmail.com>
2023-09-24 13:31:10 -0300

    feat: cria elementos da tela Signup

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,6 +1,26 @@
 function SignUp() {
     return (
-        <h1>SignUp</h1>
+        <>
+            <h1>Hello!</h1>
+            <p>Signup to get Started</p>
+            <form>
+                <label htmlFor="username">
+                    Username
+                    <input type="text" id="username" />
+                </label>
+                <label htmlFor="password">
+                    Password
+                    <input type="text" id="password" />
+                </label>
+                <label htmlFor="remembermy">
+                    <input type="checkbox" name="remembermy" id="remembermy" />
+                    Remember my
+                </label>
+                <a href="/forgotpassword">Forgot the password ?</a>
+                <button>login</button>
+                <span>Already have an account ? <a href="/">Login</a></span>
+            </form>
+        </>
     )
 }
 
