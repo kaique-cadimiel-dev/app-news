7439c8d86ab1a8e940f6d50cc0af7e135c06bb89
kaique-cadimiel-dev <kaiqueamasio@gmail.com>
2023-09-24 13:40:45 -0300

    feat: cria elementos da tela Verification

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from '../src/pages/login/Login.tsx';
 import ForgotPassword from '../src/pages/forgotpassword/Forgotpassword.tsx';
 import SignUp from '../src/pages/signup/Signup.tsx';
+import Verification from '../src/pages/verification/Verification.tsx';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/forgotpassword" element={<ForgotPassword />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/verification" element={<Verification />} />
     </Routes>
   )
 }
